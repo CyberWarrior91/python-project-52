fb80706500642dba4093516ef097fc6a46694cb4
Vladislav25 <vladkhleborodov@gmail.com>
2023-06-15 17:00:24 +0300

    remake template, add translations

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 dev:
 	poetry run python manage.py runserver
-PORT ?= 8000
-start:
-		poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
+translate:
+	poetry run python manage.py makemessages -l ru_RU
+compile:
+	poetry run python manage.py compilemessages
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,10 +1,28 @@
 # *task_manager/views.py*
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+import logging
+from django.utils.translation import gettext as _
+
+
+logger = logging.getLogger(__name__)
+
+file_handler = logging.FileHandler('errors.log')
+file_handler.setLevel(logging.ERROR)
+
+logger.addHandler(file_handler)
+
 
 class HomePageView(TemplateView):
 
     template_name = 'index.html'
     
     def index(self, request):
+        
+        logger.error('This is an error message')
         return render(request, HomePageView.template_name)
+
+def users(request):
+    output = _('Users')
+    return render(request, 'users.html', context={'output': output})
+
