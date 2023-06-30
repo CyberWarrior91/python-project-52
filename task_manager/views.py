4a88c664c38d8dd9f63b722d0270a6167180896d
Vladislav25 <vladkhleborodov@gmail.com>
2023-06-30 18:48:05 +0300

    modify forms and views for user create

--- task_manager/views.py
+++ task_manager/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 import logging
 from django.utils.translation import gettext as _
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 
 logger = logging.getLogger(__name__)
@@ -23,8 +26,19 @@ class HomePageView(TemplateView):
         return render(request, HomePageView.template_name)
 
 
+class UserLoginView(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('main')
+
+    def form_invalid(self, form):
+        messages.error(self.request,'Invalid username or password')
+        return self.render_to_response(self.get_context_data(form=form))
+
+
 def users_create(request):
     return render(request, 'registration.html')
 
-def login(request):
-    return render(request, 'login.html')
+
