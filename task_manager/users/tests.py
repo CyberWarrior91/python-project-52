56daf2bff7ede42d5b69bf5ec2506a8df178d819
Vladislav25 <vladkhleborodov@gmail.com>
2023-07-28 16:18:10 +0300

    add login mixins

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
-
+from .models import CustomUser
+from django.core.management import call_command
 # Create your tests here.
+
+class UserTestCase(TestCase):
+    
+    fixtures = ['dumpdata.json']
+
+    def setUp(self):
+        # Load fixtures
+        call_command('loaddata', *self.fixtures)
+
+    def test_change_user(self):
+        user = CustomUser.objects.get(username='Billy333')
+        user.first_name = 'Bob'
+        user.save()
+        self.assertEqual(user.first_name, 'Bob')
+
+    def test_delete_user(self):
+        user = CustomUser.objects.get(username='Billy333')
+        user.delete()
+        self.assertRaises(CustomUser.DoesNotExist, CustomUser.objects.get, username='Billy333')
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
@@ -6,14 +8,24 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import NewUserForm, UserUpdateForm
 from django.contrib import messages
 from .models import CustomUser
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 from django.contrib.auth.forms import UserChangeForm
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.models import AbstractUser
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+class UserLoginMixin(LoginRequiredMixin):
+    permission_denied_message = _('You are not authorized! Please login to the system.')
+    permission_denied_url = reverse_lazy('login')
+
+    def handle_no_permission(self) -> HttpResponseRedirect:
+        messages.error(self.request, self.get_permission_denied_message(), extra_tags='danger')
+        return redirect(self.permission_denied_url)
+
+
 class UserList(ListView):
     model = CustomUser
     template_name = 'users/index.html'
@@ -36,16 +48,21 @@ class UserCreateView(View):
         else:
             return render(request, 'users/user_create.html', {'form': form})
 
-class UserUpdateView(PermissionRequiredMixin, View):
+class UserUpdateView(UserLoginMixin, View):
     success_url = '/users/'
-    permission_required = 'users.change_customuser'
-
+    
     def get(self, request, *args, **kwargs):
-        user_id = kwargs.get('pk')
-        user = CustomUser.objects.get(pk=user_id)
-        form = UserUpdateForm(instance=user)
-        return render(request, 'users/user_update.html', context={
-            'form': form, 'user_id':user_id, })
+        user_id = request.user.pk
+        page_id = kwargs.get('pk')
+        if user_id == page_id:
+            user = CustomUser.objects.get(pk=user_id)
+            form = UserUpdateForm(instance=user)
+            return render(request, 'users/user_update.html', context={
+                'form': form, 'user_id':user_id, })
+        else:
+            messages.error(request, _('You have no rights to modify another user.'), extra_tags='danger')
+            return redirect(self.success_url)
+    
 
     def post(self, request, *args, **kwargs):
         user_id = kwargs.get('pk')
@@ -59,10 +76,24 @@ class UserUpdateView(PermissionRequiredMixin, View):
             return render(request, 'users/user_update.html', {'form': form, 'user_id':user_id,})
 
 
-class UserDeleteView(DeleteView):
+class UserDeleteView(UserLoginMixin, DeleteView):
     template_name = 'users/user_delete.html'
     model = CustomUser
     success_url = '/users/'
+
+    def get(self, request, *args, **kwargs):
+        user_id = request.user.pk
+        page_id = kwargs.get('pk')
+        if user_id == page_id:
+            self.object = self.model.objects.get(pk=user_id)
+            form = self.get_form()
+            if form.is_valid():
+                return self.form_valid(form)
+            else:
+                return self.form_invalid(form)
+        else:
+            messages.error(request, _('You have no rights to modify another user.'), extra_tags='danger')
+            return redirect(self.success_url)
     
     def form_valid(self, form):
         self.object.delete()
