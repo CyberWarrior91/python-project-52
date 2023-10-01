4b9ed3e090ec042ff0ab0e947eda32b8f9c127dc
Vladislav <vladkhleborodov@gmail.com>
2023-10-01 13:15:55 +0300

    add bootstrap5

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -225,16 +225,18 @@ CSRF = os.getenv('HTTPS', None)
 CSRF_TRUSTED_ORIGINS = ['https://python-project-52-production-882e.up.railway.app']
 
 BOOTSTRAP5 = {
-
-    # The complete URL to the Bootstrap CSS file
-    # Note that a URL can be either a string,
-    # e.g. "https://stackpath.bootstrapcdn.com/bootstrap/5.1.1/css/bootstrap.min.css",
-    # or a dict like the default value below.
+    # The complete URL to the Bootstrap CSS file.
     "css_url": {
-        "href":
-        "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
-        "integrity":
-        "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB",
+        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css",  # noqa: E501
+        "integrity": "sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ",  # noqa: E501
         "crossorigin": "anonymous",
     },
+
+    # The complete URL to the Bootstrap bundle JavaScript file.
+    "javascript_url": {
+        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js",  # noqa: E501
+        "integrity": "sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe",  # noqa: E501
+        "crossorigin": "anonymous",
+    },
+    'javascript_in_head': True,
 }
